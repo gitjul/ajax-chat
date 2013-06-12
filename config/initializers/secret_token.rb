757b103c6c32d84f91883d476ffeefa4ea22ce2a
Julia Jacobs <julia@llp.pl>
2013-06-12 14:29:17 +0200

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxChat::Application.config.secret_token = '75129c09ac47e53b85b8089390c7e7c98690c670c860ff8042213db4cc380ec6454da96ec94907ef1215d767656b20b20f7d1dc03180a4a19463726411a37645'
