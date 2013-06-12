48f50ba4c0a21dc44c6987c78fab6881149e4e7a
Julia Jacobs <julia@llp.pl>
2013-06-12 17:05:20 +0200

    Prepare a view, route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ AjaxChat::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'chat#index'
 
   # See how all your routes lay out with "rake routes"
 
